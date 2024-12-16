5826f55cf0f1adb2d35fe4a1ccc9c2a2e6052d9b
Emerson de Araujo pinho <the.emerson.araujo@gmail.com>
2024-12-16 11:49:37 -0300

    estilizando o view

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,19 @@ import { Text, View, StyleSheet } from "react-native"
 
 export default function Index(){
     return (
-        <view>
-            <Text style={style.title}>Hello react native!</Text>
+        <view style={style.container}>
+            <Text style={style.title}>Hello</Text>
+            <Text style={style.title}>react native!</Text>
         </view>  
     )
 }
 
 const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
     title: {
         color: "red",
         fontSize: 22,
