b7f2a3465f74b8d38cf3359884dbb2446f96ff1a
Emerson de Araujo pinho <the.emerson.araujo@gmail.com>
2024-12-19 10:06:42 -0300

    adicionando o material icons

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,18 @@
 import { View, Image } from "react-native";
 
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { style } from "./style";
+import { colors } from "@/app/styles/colors";
 
 export default function Index(){
     return (
         <View style={style.container}>
             <View style={style.header}>
                 <Image source={require("@/assets/logo.png")} style={style.logo} />
+
+
+                <MaterialIcons name="add" size={32} color={colors.green[300]} />
             </View>
         </View>  
     )
