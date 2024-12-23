f12eb3030c3075d85f32cd9227eec3add8deada5
Emerson de Araujo pinho <the.emerson.araujo@gmail.com>
2024-12-23 13:31:58 -0300

    refatorando o componente Category para aceitar propriedades de Pressable

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,18 +1,17 @@
-import { Text, Pressable } from "react-native";
+import { Text, Pressable, PressableProps} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { colors } from "@/app/styles/colors";
 import { styles } from "./style";
 
-type props = {
+type props = PressableProps & {
   name: string;
   icon: keyof typeof MaterialIcons.glyphMap
-  onPress?: () => void;
 }
 
-export function Category({name, icon, onPress}: props) {
+export function Category({name, icon, ...rest}: props) {
   return (
-    <Pressable style={styles.container} onPress={onPress}>
+    <Pressable style={styles.container} {...rest}>
       <MaterialIcons name={icon} size={16} color={colors.gray[400]} />
       <Text style={styles.name}>{name}</Text>
     </Pressable>
