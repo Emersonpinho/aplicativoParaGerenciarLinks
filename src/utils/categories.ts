ea9792357baf858ba2c4fa085947fc62dbcd7e2f
Emerson de Araujo pinho <the.emerson.araujo@gmail.com>
2024-12-24 11:43:16 -0300

    adicionando o arquivo de categorias com definição de tipo e exemplo de categoria

---
+++ src/utils/categories.ts
@@ -0,0 +1,12 @@
+import { MaterialIcons } from "@expo/vector-icons";
+
+type Category = {
+    id: string
+    name: string
+    icon: keyof typeof MaterialIcons.glyphMap
+}
+
+
+export const categories: Category[] = [
+    { id: "1", name: "curso", icon: "code" },
+]
