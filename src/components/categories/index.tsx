34b5beac65e04b8e525a63e096d1ee079b857fcb
Emerson de Araujo pinho <the.emerson.araujo@gmail.com>
2024-12-26 21:40:36 -0300

    adicionando componente Categories com FlatList para renderizar categorias

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -2,4 +2,15 @@ import { FlatList } from 'react-native';
 
 import { styles } from "./style";
 import { categories } from "@/utils/categories";
-import { Category } from "@/components/category";   
+import { Category } from "@/components/category";
+
+export function Categories() {
+    return (
+    <FlatList 
+    data={categories}
+    keyExtractor={(item) => item.id}
+    renderItem={({ item }) => (
+    <Category name={item.name} icon={item.icon} isSelect={false} /> )}
+  />
+)  
+}
