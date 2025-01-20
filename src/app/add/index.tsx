adee46a4e92c281c8e0919956fb642a60adb67fc
Emerson <the.emerson.araujo@gmail.com>
2025-01-20 11:13:32 -0300

    feat: add TouchableOpacity and implement basic structure for add component

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,5 +1,16 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/app/styles/colors"
+import { style } from "../index/style"
+
+export default function add(){
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                
+            </View>
+        </View>
+    )
+}
