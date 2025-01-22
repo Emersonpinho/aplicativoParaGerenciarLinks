0964e373ce81515af2d96155fe941c7df1f2f209
Emerson <the.emerson.araujo@gmail.com>
2025-01-22 12:09:56 -0300

    feat: create input component with custom styles

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,1 +1,9 @@
 import { TextInput, TextInputProps } from "react-native"
+import { styles } from "./styles"
+
+
+export function input({ ...rest}: TextInputProps){
+    return (
+        <TextInput style={styles.container} {...rest} />
+    )
+}
