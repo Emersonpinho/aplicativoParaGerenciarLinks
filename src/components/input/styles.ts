86366ea24501352280c37e3a9a030fdd279d92b0
Emerson <the.emerson.araujo@gmail.com>
2025-01-22 12:05:39 -0300

    feat: add styles for input component container

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,3 +1,16 @@
 import { StyleSheet } from "react-native"
 import { colors } from "@/app/styles/colors"
 
+export const styles = StyleSheet.create({
+    container: {
+        height: 52,
+        width: "100%",
+        backgroundColor:colors.gray[900],
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: colors.gray[800],
+        padding: 10,
+        color: colors.gray[100],
+        fontSize: 16,
+    }
+})
