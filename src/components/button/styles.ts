7fc8e94fb43a43f21f89afd3dec8f923020d3a53
Emerson <the.emerson.araujo@gmail.com>
2025-01-24 09:56:34 -0300

    criando a estilização

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -3,6 +3,11 @@ import { colors } from "@/app/styles/colors"
 
 export const styles = StyleSheet.create({
     container: {
-        
+        height: 52,
+        width: "100%",
+        backgroundColor: colors.gray[300],
+        borderRadius: 8,
+        justifyContent: "center",
+        alignItems: "center",
     }
 })
