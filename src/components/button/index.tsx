940ada769020bda1ea53654e8ea60fd3b3fdadba
Emerson <the.emerson.araujo@gmail.com>
2025-01-24 10:07:23 -0300

    feat: add activeOpacity prop to TouchableOpacity for improved feedback

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -8,7 +8,7 @@ type Props = TouchableOpacityProps & {
 
 export function Button({ title, ...rest}: Props){
      return (
-        <TouchableOpacity style={styles.container} {...rest}>
+        <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
             <Text style={styles.title}>{title}</Text>
         </TouchableOpacity>
      )
