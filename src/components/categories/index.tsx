4649d017a04e9c2f354559592ba6f187b059cfc5
Emerson <the.emerson.araujo@gmail.com>
2025-01-30 20:45:54 -0300

    refactor: standardize import statements in Categories component

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,8 +1,8 @@
-import { FlatList } from 'react-native';
+import { FlatList } from 'react-native'
 
-import { styles } from "./style";
-import { categories } from "@/utils/categories";
-import { Category } from "@/components/category";
+import { styles } from "./style"
+import { categories } from "@/utils/categories"
+import { Category } from "@/components/category"
 
 type Props = {
   selected: string
