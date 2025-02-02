7a34f1bd358e4d26aff1fcb10448d3e38a61df8e
Emerson <the.emerson.araujo@gmail.com>
2025-02-02 09:54:19 -0300

    feat: define LINK_STORAGE_KEY and LinkStorage type in link-storage.ts

--- src/storage/link-storage.ts
+++ src/storage/link-storage.ts
@@ -1,2 +1,12 @@
 import  AsyncStorage  from "@react-native-async-storage/async-storage"
 
+const LINKS_STORAGE_KEY = 'Links-storage'
+
+
+type LinkStorage = {
+    id: string
+    name: string
+    url: string
+    category: string
+}
+
