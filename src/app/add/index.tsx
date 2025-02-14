0af4bf7891abe6d017ef2644f7b5b1d8a88fa3d4
Emerson <the.emerson.araujo@gmail.com>
2025-02-14 16:03:53 -0300

    feat: add error handling for link saving in Add component

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -38,6 +38,7 @@ export default function Add() {
             url,
             category,
         })
+        
     } catch (error){
         Alert.alert("Erro", "Não foi possível salvar o link!!")
         console.log(error)
